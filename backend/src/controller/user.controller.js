ca29a0e7a86d2dbf73d4473cda6022d71301e0f9
Ashithdeveloper <ashithashith593@gmail.com>
2025-09-13 11:43:39 +0530

    login function added

--- backend/src/controller/user.controller.js
+++ backend/src/controller/user.controller.js
@@ -191,3 +191,23 @@ export const getme = async(req, res) => {
   }
 }
 
+export const login = async(req, res) => {
+  try {
+    const { email, password } = req.body;
+    if( !email || !password){
+      return res.status(400).json({message : "All fields are required"});
+    }
+    const user = await User.findOne({ email });
+    if(!user){
+      return res.status(400).json({message : "User not found"});
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch){
+      return res.status(400).json({message : "Invalid credentials"});
+    }
+    const token = generateToken(user._id);
+    return res.status(200).json({message : "User logged in successfully", token});
+  } catch (error) {
+    
+  }
+}
