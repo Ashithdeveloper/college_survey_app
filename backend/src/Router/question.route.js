8b321247fec9f4967e6f10cd05a325fc0be28773
Ashithdeveloper <ashithashith593@gmail.com>
2025-09-13 17:03:40 +0530

    gog

--- backend/src/Router/question.route.js
+++ backend/src/Router/question.route.js
@@ -1,11 +1,12 @@
 import express from "express";
 import verifyToken from "../middleware/token.middleware.js";
-import { getquestion, saveanswer } from "../controller/question.controller.js";
+import { getquestion, getresult, saveanswer } from "../controller/question.controller.js";
 
 const router = express.Router();
 
 
 router.get("/",verifyToken, getquestion)
 router.post("/answer",verifyToken,saveanswer);
+router.get('/result',getresult);
 
 export default router
